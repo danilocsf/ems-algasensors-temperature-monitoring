2045bff71cf393c62ab557af1f5bcf9b19f6ac9d
Danilo Ferreira <danilocsf@gmail.com>
2025-10-11 14:09:28 -0300

    Criação de de Binding e Queue

---
+++ src/main/java/com/algaworks/algasensors/temperature/monitoring/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,29 @@
+package com.algaworks.algasensors.temperature.monitoring.infrastructure.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder.durable("temperature-monitoring.process-temperature.v1.q").build();
+    }
+
+    @Bean
+    public Binding binding() {
+        /*Essa exchange foi craida no processing*/
+        FanoutExchange fannoutExchange = ExchangeBuilder.fanoutExchange("temperature-processing.temperature-received.v1.e").build();
+        return BindingBuilder.bind(queue()).to(fannoutExchange);
+    }
+
+}
